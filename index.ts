cf4d4da045853c83fe05759b70460a987f48246d
Jason Buckner <jason@jasonbuckner.com>
2021-11-17 08:31:27 -0800

    Update dependencies and add mock resize observer for testing

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ export {
   SharedResizeObserverInterface,
   SharedResizeObserverResizeHandlerInterface,
 } from './src/shared-resize-observer';
+
+export { MockSharedResizeObserver } from './test/mock-shared-resize-observer';
